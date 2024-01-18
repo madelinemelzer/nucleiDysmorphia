9f1677a639355b3503a41e8208af26b968d8241b
madelinemelzer <madelinemelzer22@gmail.com>
2024-01-17 23:59:28 -0600

    metric fixes and finalization, final seg and plots

--- metricPlots.R
+++ metricPlots.R
@@ -1,5 +1,5 @@
 ####script for Salvador et al. Analysis of nuclear dysmorphia
-####created by Madeline Melzer 20230731, updated by Madeline E Melzer 20231221
+####created by Madeline Melzer 20230731, updated by Madeline E Melzer 20240117
 ####adapted from 2023Feb24_NuclearMetricPlots
 
 library(ggplot2)
@@ -15,161 +15,158 @@ library(factoextra)
 
 set.seed(23)
 
-## defining the directories
-dataDirectory <- "/Volumes/fsmresfiles/Basic_Sciences/CDB/CDB_Collaborations/Arispe_Goyal/MadelineMelzer/DATA/nucleiDysmorphia/data/age/output_metrics/"
-plotDirectory <- "/Volumes/fsmresfiles/Basic_Sciences/CDB/CDB_Collaborations/Arispe_Goyal/MadelineMelzer/DATA/nucleiDysmorphia/plots/20231221_finalPlots/"
+### defining the directories
+getwd()
+dataDirectory <- "/Volumes/fsmresfiles/Basic_Sciences/CDB/CDB_Collaborations/Arispe_Goyal/MadelineMelzer/DATA/nucleiDysmorphia/data/age_final/results/metricTables/"
+saveDirectory <- "/Volumes/fsmresfiles/Basic_Sciences/CDB/CDB_Collaborations/Arispe_Goyal/MadelineMelzer/DATA/nucleiDysmorphia/data/age_final/results/umapTables/"
+plotDirectory <- "/Volumes/fsmresfiles/Basic_Sciences/CDB/CDB_Collaborations/Arispe_Goyal/MadelineMelzer/DATA/nucleiDysmorphia/plots/final/"
 
-## loading the data
-data = as_tibble(read.table(paste0(dataDirectory, "20231221_allAges.csv"), stringsAsFactors=F, header = T, sep = ","))
+### loading the data
+data = as_tibble(read.table(paste0(dataDirectory, "20240117_allAges.csv"), stringsAsFactors=F, header = T, sep = ","))
 
-## preprocessing
-control = data %>% select(-centroid.0,-centroid.1)%>% mutate(concavity_ct.log = log(concavity_ct+1), area_norm = 3.14*(major_axis_length*minor_axis_length)/4, dArea = area_norm-area)
+### applying area threshold
+lower_bound =  5502.375
+upper_bound =  17819.375
+data = data %>% filter(area >= lower_bound & area <= upper_bound & location != "" & !is.na(pointRadiusRMSDifference)) #added lower and upper bounds from 1.5 IQR area, cross check with metrics.ipynb result
 
-## area cutoffs
-lower_bound =  5509.125
-upper_bound =  17812.125
-controlCombined = control %>% filter(area >= lower_bound & area <= upper_bound & location != "") #added lower and upper bounds from 1.5 IQR area, calcualted in metrics jupyter notebook
+### processing data for dimensional reduction with PCA and UMAP
+data <- data %>% mutate(ID=row_number()) #the ID is to link the UMAP data with the metadata
 
-############################################################################################
-############################## Dimensional Plotting ########################################
-############################################################################################
+metrics = data %>% select('area',
+                          'perimeter',
+                          'major_axis_length',
+                          'minor_axis_length',
+                          'eccentricity',
+                          'solidity',
+                          'equivalent_diameter_area',
+                          'area_bbox',
+                          'area_convex',
+                          'extent',
+                          'feret_diameter_max',
+                          'perimeter_crofton',
+                          'moments_hu.0',
+                          'moments_hu.1',
+                          'moments_hu.2',
+                          'moments_hu.3',
+                          'moments_hu.4',
+                          'moments_hu.5',
+                          'moments_hu.6',
+                          'circularity',
+                          'aspectRatio',
+                          'flattening',
+                          'averagePointRadius',
+                          'pointRadiusExtremeDifference',
+                          'pointRadiusRMSDifference',
+                          'areaEquivalentEllipse',
+                          'perimeterEquivalentEllipse',
+                          'dArea',
+                          'dPerimeter',
+                          'dAreaConvex',
+                          'xInterceptCount',
+                          'concavityCount',
+                          'ID')
+                          
+  
+metadata = data %>% select('file',
+                           'label',
+                           'age',
+                           'sex',
+                           'animal',
+                           'location',
+                           'area',
+                           'perimeter',
+                           'major_axis_length',
+                           'minor_axis_length',
+                           'eccentricity',
+                           'solidity',
+                           'equivalent_diameter_area',
+                           'area_bbox',
+                           'area_convex',
+                           'extent',
+                           'feret_diameter_max',
+                           'perimeter_crofton',
+                           'moments_hu.0',
+                           'moments_hu.1',
+                           'moments_hu.2',
+                           'moments_hu.3',
+                           'moments_hu.4',
+                           'moments_hu.5',
+                           'moments_hu.6',
+                           'circularity',
+                           'aspectRatio',
+                           'flattening',
+                           'averagePointRadius',
+                           'pointRadiusExtremeDifference',
+                           'pointRadiusRMSDifference',
+                           'areaEquivalentEllipse',
+                           'perimeterEquivalentEllipse',
+                           'dArea',
+                           'dPerimeter',
+                           'dAreaConvex',
+                           'xInterceptCount',
+                           'concavityCount',
+                           'ID')
 
-controlCombined <- controlCombined %>% mutate(ID=row_number())
+metrics.noID = metrics %>% select(-ID)
+metrics.noID = na.omit(metrics.noID)
 
-controlCombined.metrics = controlCombined %>% select(eccentricity,
-                                                     perimeter,
-                                                     dArea,
-                                                     area,
-                                                     major_axis_length,
-                                                     minor_axis_length,
-                                                     r_avg,
-                                                     rms_avg,
-                                                     rms_subsampled,
-                                                     x_intercepts,
-                                                     concavity_ct,
-                                                     circularity,
-                                                     solidity,
-                                                     equivalent_diameter_area,
-                                                     area_bbox,
-                                                     area_convex,
-                                                     area_filled,
-                                                     extent,
-                                                     feret_diameter_max,
-                                                     compactness,
-                                                     roundness,
-                                                     convexity,
-                                                     aspect_ratio,
-                                                     concavity_ct.log,
-                                                     perimeter_crofton) %>% mutate(ID = row_number())
+metrics.pca = prcomp(metrics.noID, scale = TRUE)
+metrics.pca$rotation = -1*metrics.pca$rotation
 
-controlCombined.meta = controlCombined %>% select(file,
-                                                     label,
-                                                     age,
-                                                     sex,
-                                                     animal,
-                                                     location,
-                                                     eccentricity,
-                                                     perimeter,
-                                                     dArea,
-                                                     area,
-                                                     major_axis_length,
-                                                     minor_axis_length,
-                                                     r_avg,
-                                                     rms_avg,
-                                                     rms_subsampled,
-                                                     x_intercepts,
-                                                     concavity_ct,
-                                                     circularity,
-                                                     solidity,
-                                                     equivalent_diameter_area,
-                                                     area_bbox,
-                                                     area_convex,
-                                                     area_filled,
-                                                     extent,
-                                                     feret_diameter_max,
-                                                     compactness,
-                                                     roundness,
-                                                     convexity,
-                                                     aspect_ratio,
-                                                     concavity_ct.log,
-                                                     perimeter_crofton) %>% mutate(ID = row_number())
+## if you want to save the PCA results with the metadata:
+#data.pca.meta = as.data.frame(metrics.pca$x) %>% inner_join(as.data.frame(metadata), by = character(0))
+#write.csv(data.pca.meta, paste0(saveDirectory, "20240115_pca.csv"))
 
-# getting rid of id column so the UMAP does not include this identifier
-controlCombined.metrics.noID = controlCombined.metrics %>% select(-ID)
-controlCombined.metrics.noID = na.omit(controlCombined.metrics.noID)
+### looking at PCAs to see how much variance is explained
+metrics.pca.variances <- metrics.pca$sdev^2
+cumulative_variances <- cumsum(metrics.pca.variances) / sum(metrics.pca.variances) # Compute cumulative variance
 
-# PCA and saving PCA
-controlCombined.metrics.pca = prcomp(controlCombined.metrics.noID, scale = TRUE)
-controlCombined.metrics.pca$rotation = -1*controlCombined.metrics.pca$rotation
-#controlCombined.pca.meta = as.data.frame(controlCombined.metrics.pca$x) %>% inner_join(as.data.frame(controlCombined.meta), by = character(0))
-#write.csv(controlCombined.pca.meta, "/Volumes/fsmresfiles/Basic_Sciences/CDB/CDB_Collaborations/Arispe_Goyal/MadelineMelzer/DATA/nucleiDysmorphia/data/age/output_metrics/UMAPs/20230918_age01_pca.csv")
+plot(metrics.pca$sdev^2, type = "b", xlab = "Principal Component", ylab = "Eigenvalue")
+plot(cumulative_variances, type = "b", xlab = "Number of Components", ylab = "Cumulative Proportion of Variance Explained")
+screeplot(metrics.pca, type = "line", npcs = 20)
 
-# elbow plot for pca above, if desired, to help pick number of PCs
-explained_variance <- (controlCombined.metrics.pca$sdev)^2 / sum((controlCombined.metrics.pca$sdev)^2)
-pca_data <- data.frame(PC = 1:length(explained_variance), Explained_Variance = explained_variance)
 
-# Create the elbow plot
-ggplot(pca_data, aes(x = PC, y = Explained_Variance)) +
-  geom_line() +
-  geom_point() +
-  theme_classic(base_size = 18) +
-  labs(x = "Principal Component", y = "Explained Variance")
-#plot
-#ggsave(plot, file = paste0("/Volumes/fsmresfiles/Basic_Sciences/CDB/CDB_Collaborations/Arispe_Goyal/MadelineMelzer/DATA/nucleiDysmorphia/plots/", '20230918_explainedVariance.png'), width = 6, height = 4)
+### choosing number of PCAs to use for UMAP based on screeplot for variance
+data.pca.umap = umap(metrics.pca$x[,1:11])
 
-# UMAP based on PCA
-controlCombined.pca.umap = umap(controlCombined.metrics.pca$x[,1:11])
-controlCombined.umap.df = controlCombined.pca.umap$layout %>%
+data.umap.df = data.pca.umap$layout %>%
   as.data.frame() %>%
   setNames(c("UMAP1", "UMAP2")) %>%
   #rename(UMAP1="X1", UMAP2="X2") %>%
   mutate(ID = row_number()) %>%
-  inner_join(controlCombined.meta, by = "ID")
-write.csv(controlCombined.umap.df, "/Volumes/fsmresfiles/Basic_Sciences/CDB/CDB_Collaborations/Arispe_Goyal/MadelineMelzer/DATA/nucleiDysmorphia/data/age/output_metrics/UMAPs/20231221_umap_allAges.csv")
+  inner_join(metadata, by = "ID")
+write.csv(data.umap.df, paste0(saveDirectory, "20240117_umap_allMetrics.csv"))
 
 
-### dot plotting UMAP
-controlCombined.umap.df.plot = controlCombined.umap.df %>% filter(age == 24)
-plot = ggplot(controlCombined.umap.df.plot, aes(x = UMAP1, y = UMAP2, color = as.factor(location))) +
-  geom_point(size = 3) +
+
+### dot plots, UMAP
+data.umap.df.plot = data.umap.df %>% filter(age == 52)
+plot = ggplot(data.umap.df.plot, aes(x = UMAP1, y = UMAP2, color = location)) +
+  geom_point(size = 2, shape = 16) +
   theme_void() +
-  theme(legend.position = "none") +
+  theme(legend.position = "none")+
   scale_color_manual(values = c("Arch" = "#6b6ca3", "Desc" = "#c93f55"))
 plot
-ggsave(plot, file = paste0(plotDirectory, '52wk_dots.svg'), width = 6, height = 4)
-ggsave(plot, file = paste0(plotDirectory, '52wk_dots.png'), width = 6, height = 4)
+ggsave(plot, file = paste0(plotDirectory, 'dot_52wk.svg'), width = 6, height = 4)
+ggsave(plot, file = paste0(plotDirectory, 'dot_52wk.png'), width = 6, height = 4)
 
-### density plot UMAP
-
-#controlCombined.umap.df.plot = controlCombined.umap.df %>% filter(age == 52)
+### density plots, UMAP
 plot = ggplot() +
-  geom_point(data = controlCombined.umap.df.plot, aes(x = UMAP1, y = UMAP2), size = 1, color = "grey93") +
-  geom_density_2d(data = controlCombined.umap.df.plot, aes(x = UMAP1, y = UMAP2, color = location), contour_var = "ndensity") +
+  geom_point(data = data.umap.df, aes(x = UMAP1, y = UMAP2), size = 1, shape = 16, color = "grey93") +
+  geom_density_2d(data = data.umap.df.plot, aes(x = UMAP1, y = UMAP2, color = location), contour_var = "ndensity") +
   theme_void() +
   theme(legend.position = "none") +
   scale_color_manual(values = c("Arch" = "#6b6ca3", "Desc" = "#c93f55"))
 plot
-ggsave(plot, file = paste0(plotDirectory, '52wk_density.svg'), width = 6, height = 4)
-ggsave(plot, file = paste0(plotDirectory, '52wk_density.png'), width = 6, height = 4)
+ggsave(plot, file = paste0(plotDirectory, 'density_52wk.svg'), width = 6, height = 4)
+ggsave(plot, file = paste0(plotDirectory, 'density_52wk.png'), width = 6, height = 4)
 
 
-#### counting number of cells for each age
-age_counts <- controlCombined.umap.df %>%
+
+### counting number of cells for each age, aortic location
+age_counts <- data.umap.df %>%
   group_by(age, location) %>%
+  #group_by(location) %>%
   summarise(count = n())
+
 print(age_counts)
-
-
-### plotting just the PCs
-controlCombined.metrics.pca.df <- data.frame(controlCombined.metrics.pca$x, ID = seq_len(nrow(controlCombined.metrics.pca$x)))
-controlCombined.metrics.pca.df <- inner_join(controlCombined.metrics.pca.df, controlCombined.meta, by = "ID")
-
-plot = ggplot(controlCombined.metrics.pca.df, aes(x = PC1, y = PC2, color = location)) +
-  geom_point() +
-  theme_classic(base_size = 18) +
-  #theme(strip.background = element_blank(), strip.text.x = element_blank()) +
-  scale_color_manual(values = c(Arch = "#762A83", Desc = "#DFE725FF")) +
-  labs(x = "PC1", y = "PC2")
-#ggsave(plot, file = paste0("/Volumes/fsmresfiles/Basic_Sciences/CDB/CDB_Collaborations/Arispe_Goyal/MadelineMelzer/DATA/nucleiDysmorphia/plots/", '20230919_pca_location.svg'), width = 6, height = 4)
-#ggsave(plot, file = paste0("/Volumes/fsmresfiles/Basic_Sciences/CDB/CDB_Collaborations/Arispe_Goyal/MadelineMelzer/DATA/nucleiDysmorphia/plots/", '20230919_pca_location.png'), width = 6, height = 4)
-plot
